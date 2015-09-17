ce5da14d309a2d1b2d8f496a1e6ee3d664b07f9e
Roberto Mirabella <bronzetto@outlook.com>
2015-09-17 15:01:14 +0200

    Added M:M association

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150914092733) do
+ActiveRecord::Schema.define(version: 20150917125154) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "argument"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "content"
     t.integer  "user_id"
+    t.integer  "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
